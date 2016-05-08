0208ea885537f762752479447e34970ae6429f7b
Harry Lam <harrylf2@live.hk>
2016-05-08 15:35:50 +0800

    Update zh-TW translations
    very coarse at this point, will refine later.

--- Languages/zh-TW.ts
+++ Languages/zh-TW.ts
@@ -6,192 +6,192 @@
     <message>
         <location filename="../configuration.ui" line="32"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>選項</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="42"/>
         <source>Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation>啟動器</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="50"/>
         <source>RCT2 Location:</source>
-        <translation type="unfinished"></translation>
+        <translation>模擬樂園2(RCT2)的位置:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="64"/>
         <source>Locate</source>
-        <translation type="unfinished"></translation>
+        <translation>尋找</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="75"/>
         <source>Branch:</source>
-        <translation type="unfinished"></translation>
+        <translation>分支:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="98"/>
         <source>Stable</source>
-        <translation type="unfinished"></translation>
+        <translation>穩定</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="108"/>
         <source>Develop</source>
-        <translation type="unfinished"></translation>
+        <translation>開發</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="159"/>
         <source>OpenRCT2 Build: n/a</source>
-        <translation type="unfinished"></translation>
+        <translation>OpenRCT2 版本: n/a</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="172"/>
         <source>OpenRCT2 Git Hash: n/a</source>
-        <translation type="unfinished"></translation>
+        <translation>OpenRCT2 Git Hash: n/a</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="199"/>
         <source>Hidden Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>隱藏設定</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="207"/>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>語言</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="318"/>
         <source>Windowed Size:</source>
-        <translation type="unfinished"></translation>
+        <translation>視窗尺寸:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="338"/>
         <source>Width</source>
-        <translation type="unfinished"></translation>
+        <translation>寬度</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="358"/>
         <source>Height</source>
-        <translation type="unfinished"></translation>
+        <translation>高度</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="380"/>
         <source>Use small font in debug console</source>
-        <translation type="unfinished"></translation>
+        <translation>在除錯命令行中使用小字體</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="392"/>
         <source>Window edge snapping distance:</source>
-        <translation type="unfinished"></translation>
+        <translation>視窗邊緣拖曳距離:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="426"/>
         <source>Don&apos;t let testing rides crash</source>
-        <translation type="unfinished"></translation>
+        <translation>禁止測試中的遊樂設施毀壞</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="436"/>
         <source>Allow loading files with incorrect checksums</source>
-        <translation type="unfinished"></translation>
+        <translation>允許載入不正確校驗值的文件</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="448"/>
         <source>Screenshot format:</source>
-        <translation type="unfinished"></translation>
+        <translation>截圖格式:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="477"/>
         <source>Always prompt for save location</source>
-        <translation type="unfinished"></translation>
+        <translation>儲存時彈出選擇框</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="487"/>
         <source>Show game intro</source>
-        <translation type="unfinished"></translation>
+        <translation>顯示遊戲片頭</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="497"/>
         <source>Hide RCT1 Mega Park when not unlocked</source>
-        <translation type="unfinished"></translation>
+        <translation>當未完成夢幻遊樂園1(RCT1)的所有劇情前, 隱藏Mega Park</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="524"/>
         <source>Multiplayer</source>
-        <translation type="unfinished"></translation>
+        <translation>多人連線模式</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="545"/>
         <source>Player Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>玩家名稱:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="590"/>
         <source>Server Name</source>
-        <translation type="unfinished"></translation>
+        <translation>伺服器名稱</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="622"/>
         <source>Server Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>伺服器密碼:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="654"/>
         <source>Server Port:</source>
-        <translation type="unfinished"></translation>
+        <translation>伺服器埠口:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="689"/>
         <source>Max number of player on server:</source>
-        <translation type="unfinished"></translation>
+        <translation>最大玩家人數:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="729"/>
         <source>Advertise server on global server list</source>
-        <translation type="unfinished"></translation>
+        <translation>將伺服器列在全球伺服器列表上</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="748"/>
         <source>Server Description:</source>
-        <translation type="unfinished"></translation>
+        <translation>伺服器描述:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="762"/>
         <source>Provider Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>提供者名稱:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="776"/>
         <source>Provider Email:</source>
-        <translation type="unfinished"></translation>
+        <translation>提供者電郵:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="790"/>
         <source>Provider Website:</source>
-        <translation type="unfinished"></translation>
+        <translation>提供者網站:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="804"/>
         <source>Master Server Override:</source>
-        <translation type="unfinished"></translation>
+        <translation>主伺服器的覆蓋參數:</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="17"/>
         <source>Launcher Version: </source>
-        <translation type="unfinished"></translation>
+        <translation>啟動器版本:</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="20"/>
         <source>OpenRCT2 Build: </source>
-        <translation type="unfinished"></translation>
+        <translation>OpenRCT2 版本:</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="23"/>
         <source>OpenRCT2 Git Hash: </source>
-        <translation type="unfinished"></translation>
+        <translation>OpenRCT2 Git Hash:</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="153"/>
         <source>Select RCT2 Install Location</source>
-        <translation type="unfinished"></translation>
+        <translation>選擇模擬樂園2(RCT2)的安裝目錄</translation>
     </message>
 </context>
 <context>
@@ -199,22 +199,22 @@
     <message>
         <location filename="../mainwindow.ui" line="32"/>
         <source>OpenRCT2 Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation>OpenRCT2啟動器</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="94"/>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>開始遊玩</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="146"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>選項</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="54"/>
         <source>Unable to Launch Game</source>
-        <translation type="unfinished"></translation>
+        <translation>無法啟動遊戲</translation>
     </message>
 </context>
 <context>
@@ -222,49 +222,49 @@
     <message>
         <location filename="../updater.cpp" line="75"/>
         <source>New Update is Available</source>
-        <translation type="unfinished"></translation>
+        <translation>可供更新</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="78"/>
         <source>There is a new update available for the launcher.</source>
-        <translation type="unfinished"></translation>
+        <translation>現有一個啟動器更新可供更新.</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="80"/>
         <source>It is recomended that you update.</source>
-        <translation type="unfinished"></translation>
+        <translation>建議你更新到該版本.</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="82"/>
         <source>Installed Version: </source>
-        <translation type="unfinished"></translation>
+        <translation>已安裝版本:</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="85"/>
         <source>Latest Version: </source>
-        <translation type="unfinished"></translation>
+        <translation>最新版本:</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="90"/>
         <source>Download Page</source>
-        <translation type="unfinished"></translation>
+        <translation>下載頁面</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="145"/>
         <location filename="../updater.cpp" line="151"/>
         <source>Invalid Download</source>
-        <translation type="unfinished"></translation>
+        <translation>不正確的下載</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="159"/>
         <location filename="../updater.cpp" line="165"/>
         <source>bin dir not created</source>
-        <translation type="unfinished"></translation>
+        <translation>並未建立bin目錄</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="177"/>
         <source>Error extracting archive</source>
-        <translation type="unfinished"></translation>
+        <translation>解壓縮錯誤</translation>
     </message>
 </context>
 </TS>
