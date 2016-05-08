09eff0d42c2248b06251ac766c4faffe2cba28da
Matte A <matte_med_latte@hotmail.com>
2016-05-08 02:31:49 +0200

    sv-SE translated
    There are still some strings that I might need to correct or change but at the same time is it useful for you to get a file in right away :p

--- Languages/sv-SE.ts
+++ Languages/sv-SE.ts
@@ -6,192 +6,192 @@
     <message>
         <location filename="../configuration.ui" line="32"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Inställningar</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="42"/>
         <source>Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="50"/>
         <source>RCT2 Location:</source>
-        <translation type="unfinished"></translation>
+        <translation>RCT2 Plats:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="64"/>
         <source>Locate</source>
-        <translation type="unfinished"></translation>
+        <translation>Hitta</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="75"/>
         <source>Branch:</source>
-        <translation type="unfinished"></translation>
+        <translation>Gren-Version:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="98"/>
         <source>Stable</source>
-        <translation type="unfinished"></translation>
+        <translation>Stabila</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="108"/>
         <source>Develop</source>
-        <translation type="unfinished"></translation>
+        <translation>Utvecklande</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="159"/>
         <source>OpenRCT2 Build: n/a</source>
-        <translation type="unfinished"></translation>
+        <translation>OpenRCT2 Version: n/a</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="172"/>
         <source>OpenRCT2 Git Hash: n/a</source>
-        <translation type="unfinished"></translation>
+        <translation>OpenRCT2 Git Version: n/a</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="199"/>
         <source>Hidden Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Dolda Inställningar</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="207"/>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Språk</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="318"/>
         <source>Windowed Size:</source>
-        <translation type="unfinished"></translation>
+        <translation>Fönsterstorlek:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="338"/>
         <source>Width</source>
-        <translation type="unfinished"></translation>
+        <translation>Bredd</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="358"/>
         <source>Height</source>
-        <translation type="unfinished"></translation>
+        <translation>Höjd</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="380"/>
         <source>Use small font in debug console</source>
-        <translation type="unfinished"></translation>
+        <translation>Använd små bokstäver i konsolen</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="392"/>
         <source>Window edge snapping distance:</source>
-        <translation type="unfinished"></translation>
+        <translation>Avståndet för fönstrets &apos;fäst&apos; funktion:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="426"/>
         <source>Don&apos;t let testing rides crash</source>
-        <translation type="unfinished"></translation>
+        <translation>Tillåt inte åkturer som testas att krascha</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="436"/>
         <source>Allow loading files with incorrect checksums</source>
-        <translation type="unfinished"></translation>
+        <translation>Tillåt filer med inkorrekta &apos;checksums&apos; att laddas</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="448"/>
         <source>Screenshot format:</source>
-        <translation type="unfinished"></translation>
+        <translation>Skärmdump format:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="477"/>
         <source>Always prompt for save location</source>
-        <translation type="unfinished"></translation>
+        <translation>Fråga alltid vart jag vill spara filer</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="487"/>
         <source>Show game intro</source>
-        <translation type="unfinished"></translation>
+        <translation>Visa spelets intro</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="497"/>
         <source>Hide RCT1 Mega Park when not unlocked</source>
-        <translation type="unfinished"></translation>
+        <translation>Göm RCT1 Mega Parken om den inte är upplåst</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="524"/>
         <source>Multiplayer</source>
-        <translation type="unfinished"></translation>
+        <translation>Flerspelare/Online</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="545"/>
         <source>Player Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Spelarens Namn:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="590"/>
         <source>Server Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Server Namn</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="622"/>
         <source>Server Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Server Lösenord:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="654"/>
         <source>Server Port:</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="689"/>
         <source>Max number of player on server:</source>
-        <translation type="unfinished"></translation>
+        <translation>Max antal spelare på servern:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="729"/>
         <source>Advertise server on global server list</source>
-        <translation type="unfinished"></translation>
+        <translation>Visa servern för alla spelare på den globala serverlistan</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="748"/>
         <source>Server Description:</source>
-        <translation type="unfinished"></translation>
+        <translation>Server Beskrivning:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="762"/>
         <source>Provider Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ägarens Namn:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="776"/>
         <source>Provider Email:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ägarens E-post:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="790"/>
         <source>Provider Website:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ägarens Hemsida:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="804"/>
         <source>Master Server Override:</source>
-        <translation type="unfinished"></translation>
+        <translation>Huvudserver Override:</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="17"/>
         <source>Launcher Version: </source>
-        <translation type="unfinished"></translation>
+        <translation>Launcherns Version:</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="20"/>
         <source>OpenRCT2 Build: </source>
-        <translation type="unfinished"></translation>
+        <translation>OpenRCT2 Version:</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="23"/>
         <source>OpenRCT2 Git Hash: </source>
-        <translation type="unfinished"></translation>
+        <translation>OpenRCT2 Git Version:</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="153"/>
         <source>Select RCT2 Install Location</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj mappen där RCT2 är Installerat</translation>
     </message>
 </context>
 <context>
@@ -199,22 +199,22 @@
     <message>
         <location filename="../mainwindow.ui" line="32"/>
         <source>OpenRCT2 Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="94"/>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>Spela</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="146"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Inställningar</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="54"/>
         <source>Unable to Launch Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Kan inte starta spelet</translation>
     </message>
 </context>
 <context>
@@ -222,49 +222,49 @@
     <message>
         <location filename="../updater.cpp" line="75"/>
         <source>New Update is Available</source>
-        <translation type="unfinished"></translation>
+        <translation>Ny Uppdatering är Tillgänglig</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="78"/>
         <source>There is a new update available for the launcher.</source>
-        <translation type="unfinished"></translation>
+        <translation>Det finns en ny uppdatering tillgänglig för launchern.</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="80"/>
         <source>It is recomended that you update.</source>
-        <translation type="unfinished"></translation>
+        <translation>Det rekommenderas att du uppdaterar.</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="82"/>
         <source>Installed Version: </source>
-        <translation type="unfinished"></translation>
+        <translation>Installerad Version: </translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="85"/>
         <source>Latest Version: </source>
-        <translation type="unfinished"></translation>
+        <translation>Senaste Version: </translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="90"/>
         <source>Download Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Hemsidan för Nedladdningen</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="145"/>
         <location filename="../updater.cpp" line="151"/>
         <source>Invalid Download</source>
-        <translation type="unfinished"></translation>
+        <translation>Ogiltig Nedladdning</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="159"/>
         <location filename="../updater.cpp" line="165"/>
         <source>bin dir not created</source>
-        <translation type="unfinished"></translation>
+        <translation>bin mappen är inte skapad</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="177"/>
         <source>Error extracting archive</source>
-        <translation type="unfinished"></translation>
+        <translation>Fel vid extrahering av filen</translation>
     </message>
 </context>
 </TS>
