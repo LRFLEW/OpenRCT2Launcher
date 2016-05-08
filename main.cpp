7b6317643b1d0a6d212c1397e0e44b0af267c3d1
LRFLEW <lrflew@aol.com>
2016-05-07 21:54:41 -0500

    Added Temporary Translation Loading for Testing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +9,9 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName(QStringLiteral("OpenRCT2"));
     QCoreApplication::setOrganizationDomain(QStringLiteral("OpenRCT2.org"));
     QCoreApplication::setApplicationName(QStringLiteral("OpenRCT2Launcher"));
+
+    QTranslator t;
+    if (t.load(QStringLiteral(":/Languages/en-US.qm"))) a.installTranslator(&t);
 
     MainWindow w;
     w.show();
