15db7b9089c5f44d6f2d7d839718108109cdab0a
Hielke Morsink <123mannetje@gmail.com>
2016-05-11 00:39:09 +0200

    Dutch translation

--- Languages/nl-NL.ts
+++ Languages/nl-NL.ts
@@ -6,192 +6,193 @@
     <message>
         <location filename="../configuration.ui" line="32"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Opties</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="42"/>
         <source>Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation>Launcher</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="50"/>
         <source>RCT2 Location:</source>
-        <translation type="unfinished"></translation>
+        <translation>RCT2 Map</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="64"/>
         <source>Locate</source>
-        <translation type="unfinished"></translation>
+        <translation>Bladeren</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="75"/>
         <source>Branch:</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Replaced the word &apos;branch&apos; with &apos;version&apos;, to make it more clear for the player.</translatorcomment>
+        <translation>Versie:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="98"/>
         <source>Stable</source>
-        <translation type="unfinished"></translation>
+        <translation>Stabiel</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="108"/>
         <source>Develop</source>
-        <translation type="unfinished"></translation>
+        <translation>Experimenteel</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="159"/>
         <source>OpenRCT2 Build: n/a</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="172"/>
         <source>OpenRCT2 Git Hash: n/a</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="199"/>
         <source>Hidden Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Verborgen instellingen</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="207"/>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Taal</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="318"/>
         <source>Windowed Size:</source>
-        <translation type="unfinished"></translation>
+        <translation>Schermresolutie</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="338"/>
         <source>Width</source>
-        <translation type="unfinished"></translation>
+        <translation>Breedte</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="358"/>
         <source>Height</source>
-        <translation type="unfinished"></translation>
+        <translation>Hoogte</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="380"/>
         <source>Use small font in debug console</source>
-        <translation type="unfinished"></translation>
+        <translation>Klein lettertype in debug console gebruiken</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="392"/>
         <source>Window edge snapping distance:</source>
-        <translation type="unfinished"></translation>
+        <translation>Uitlijn marge tussen schermen:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="426"/>
         <source>Don&apos;t let testing rides crash</source>
-        <translation type="unfinished"></translation>
+        <translation>Voorkom crashes tijdens het testen</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="436"/>
         <source>Allow loading files with incorrect checksums</source>
-        <translation type="unfinished"></translation>
+        <translation>Bestanden met incorrecte checksum toch laden</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="448"/>
         <source>Screenshot format:</source>
-        <translation type="unfinished"></translation>
+        <translation>Bestandsformaat voor screenshots:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="477"/>
         <source>Always prompt for save location</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebruik dialoogvensters altijd bij het opslaan</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="487"/>
         <source>Show game intro</source>
-        <translation type="unfinished"></translation>
+        <translation>Spelintro gebruiken</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="497"/>
         <source>Hide RCT1 Mega Park when not unlocked</source>
-        <translation type="unfinished"></translation>
+        <translation>Verberg het RCT1 Mega Park wanneer deze nog niet ontgrendeld is</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="524"/>
         <source>Multiplayer</source>
-        <translation type="unfinished"></translation>
+        <translation>Multi-player</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="545"/>
         <source>Player Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebruikersnaam:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="590"/>
         <source>Server Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Naam van de server:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="622"/>
         <source>Server Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Wachtwoord van de server:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="654"/>
         <source>Server Port:</source>
-        <translation type="unfinished"></translation>
+        <translation>Poort van de server:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="689"/>
         <source>Max number of player on server:</source>
-        <translation type="unfinished"></translation>
+        <translation>Maximum aantal spelers op de server:</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="729"/>
         <source>Advertise server on global server list</source>
-        <translation type="unfinished"></translation>
+        <translation>Server aanmelden bij de openbare serverlijst</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="748"/>
         <source>Server Description:</source>
-        <translation type="unfinished"></translation>
+        <translation>Beschrijving van de server</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="762"/>
         <source>Provider Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Naam van provider</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="776"/>
         <source>Provider Email:</source>
-        <translation type="unfinished"></translation>
+        <translation>E-mail van procider</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="790"/>
         <source>Provider Website:</source>
-        <translation type="unfinished"></translation>
+        <translation>Website van provider</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="804"/>
         <source>Master Server Override:</source>
-        <translation type="unfinished"></translation>
+        <translation>Master Server Alternatief</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="17"/>
         <source>Launcher Version: </source>
-        <translation type="unfinished"></translation>
+        <translation>Launcher versie: </translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="20"/>
         <source>OpenRCT2 Build: </source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="23"/>
         <source>OpenRCT2 Git Hash: </source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="153"/>
         <source>Select RCT2 Install Location</source>
-        <translation type="unfinished"></translation>
+        <translation>Selecteer de map waarin RCT2 geïnstalleerd is</translation>
     </message>
 </context>
 <context>
@@ -199,22 +200,22 @@
     <message>
         <location filename="../mainwindow.ui" line="32"/>
         <source>OpenRCT2 Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="94"/>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>Start OpenRCT2</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="146"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Opties</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="54"/>
         <source>Unable to Launch Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Spel kon niet worden gestart</translation>
     </message>
 </context>
 <context>
@@ -222,49 +223,49 @@
     <message>
         <location filename="../updater.cpp" line="75"/>
         <source>New Update is Available</source>
-        <translation type="unfinished"></translation>
+        <translation>Nieuwe Update Beschikbaar</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="78"/>
         <source>There is a new update available for the launcher.</source>
-        <translation type="unfinished"></translation>
+        <translation>Er is een nieuwe versie voor de launcher beschikbaar.</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="80"/>
         <source>It is recomended that you update.</source>
-        <translation type="unfinished"></translation>
+        <translation>Het wordt aanbevolen om te updaten.</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="82"/>
         <source>Installed Version: </source>
-        <translation type="unfinished"></translation>
+        <translation>Geïnstalleerde versie: </translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="85"/>
         <source>Latest Version: </source>
-        <translation type="unfinished"></translation>
+        <translation>Nieuwste versie: </translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="90"/>
         <source>Download Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Download Pagina</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="145"/>
         <location filename="../updater.cpp" line="151"/>
         <source>Invalid Download</source>
-        <translation type="unfinished"></translation>
+        <translation>Ongeldige download</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="159"/>
         <location filename="../updater.cpp" line="165"/>
         <source>bin dir not created</source>
-        <translation type="unfinished"></translation>
+        <translation>De bin map kon niet worden aangemaakt</translation>
     </message>
     <message>
         <location filename="../updater.cpp" line="177"/>
         <source>Error extracting archive</source>
-        <translation type="unfinished"></translation>
+        <translation>Fout bij uitpakken archief</translation>
     </message>
 </context>
 </TS>
