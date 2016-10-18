19857be39522cc7d24d10719c207f7a0b1eb11d1
LRFLEW <lrflew@aol.com>
2016-10-18 01:19:20 -0500

    Add 64-bit Flavors

--- platform.h
+++ platform.h
@@ -10,26 +10,32 @@
 
 #if defined(Q_OS_WIN)
 
+#ifdef Q_PROCESSOR_X86_64
+#define OPENRCT2_FLAVOR "6"
+#else
 #define OPENRCT2_FLAVOR "1"
+#endif
+
 // Qt uses forward slashes for all OS's and converts to backslashes internally
 #define OPENRCT2_BASE "OpenRCT2/"
 #define OPENRCT2_EXEC_NAME "openrct2.exe"
 
-// Workaround for QStringLiteral concatenation issue
-#define OPENRCT2_BIN "OpenRCT2/bin/"
-#define OPENRCT2_EXEC_LOCATION "OpenRCT2/bin/openrct2.exe"
-#define OPENRCT2_THEMES_LOCATION "OpenRCT2/themes"
-#define OPENRCT2_TITLE_LOCATION "OpenRCT2/title sequences"
-
 #elif defined(Q_OS_OSX)
 
+// Only one flavor for macOS
 #define OPENRCT2_FLAVOR "3"
+
 #define OPENRCT2_BASE "Library/Application Support/OpenRCT2/"
 #define OPENRCT2_EXEC_NAME "OpenRCT2.app/Contents/MacOS/OpenRCT2"
 
 #elif defined(Q_OS_LINUX)
 
+#ifdef Q_PROCESSOR_X86_64
+#define OPENRCT2_FLAVOR "9"
+#else
 #define OPENRCT2_FLAVOR "4"
+#endif
+
 #define OPENRCT2_BASE ".config/OpenRCT2/"
 #define OPENRCT2_EXEC_NAME "openrct2"
 
@@ -39,12 +45,8 @@
 
 #endif
 
-#ifndef Q_OS_WIN
 #define OPENRCT2_BIN OPENRCT2_BASE "bin/"
 #define OPENRCT2_EXEC_LOCATION OPENRCT2_BIN OPENRCT2_EXEC_NAME
-#define OPENRCT2_THEMES_LOCATION OPENRCT2_BIN "themes"
-#define OPENRCT2_TITLE_LOCATION "title sequences"
-#endif
 
 #ifdef Q_OS_WIN
 #include <QStandardPaths>
